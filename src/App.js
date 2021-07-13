b2ebd5cccaeb91737e10a2f2ad06a9385d8e9fe2
Tim Lee <tim@mocainc.com>
2021-07-14 07:23:41 +0900

    beforeFB2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import React, {useState} from 'react';
-import {Switch} from 'react-native';
+import React from 'react';
 import {StatusBar} from 'react-native';
-import styled, {ThemeProvider} from 'styled-components/native';
+import {ThemeProvider} from 'styled-components/native';
 import {theme} from './theme';
 import Navigation from './navigation';
 
-const Container = styled.View`
-  flex: 0.2;
-  background-color: ${props => props.theme.background};
-  align-items: center;
-  justify-content: center;
-`;
-
 const App = () => {
- 
   return (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle="dark-content" />
