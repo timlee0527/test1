da8c75be3ee31a5009425606e56c7e5f22c868d7
Tim Lee <tim@mocainc.com>
2021-07-16 08:45:40 +0900

    test

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -19,7 +19,11 @@ const AuthStack = () => {
         component={Login}
         options={{headerShown: false}}
       />
-      <Stack.Screen name="Signup" component={Signup} options={{headerBackTitleVisible: false}} />
+      <Stack.Screen
+        name="Signup"
+        component={Signup}
+        options={{headerBackTitleVisible: false}}
+      />
     </Stack.Navigator>
   );
 };
